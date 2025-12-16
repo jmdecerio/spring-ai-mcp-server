31cc8eb71bfc7e27e60ec7d797332b82623900f3
Jorge Martin <jorgejose.martin@cgi.com>
2025-12-16 13:08:53 +0100

    Trace to see callback from openai

--- src/main/java/com/jmdecerio/spring_ai_mcp_server/mcptools/McpTools.java
+++ src/main/java/com/jmdecerio/spring_ai_mcp_server/mcptools/McpTools.java
@@ -1,9 +1,11 @@
 package com.jmdecerio.spring_ai_mcp_server.mcptools;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springaicommunity.mcp.annotation.McpTool;
 import org.springaicommunity.mcp.annotation.McpToolParam;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class McpTools {
 
@@ -11,6 +13,7 @@ public class McpTools {
     public int add(
             @McpToolParam(description = "Primer sumando", required = true) int a,
             @McpToolParam(description = "Segundo sumando", required = true) int b) {
+        log.info("Sumando: {} + {} = {}", a, b, a+b);
         return a + b;
     }
 }
